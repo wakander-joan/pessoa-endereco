697c8772136b5b67bb43498b6d552c988a0e0515
joan <https://github.com/wakander-joan/Teste.git>
2023-01-16 20:20:42 -0300

    Primeiro Commit - Projeto iniciado

---
+++ src/main/java/br/com/attornatus/pessoaendereco/PessoaEnderecoApplication.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.pessoaendereco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaEnderecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaEnderecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/attornatus/pessoaendereco/PessoaEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.attornatus.pessoaendereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoaEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
