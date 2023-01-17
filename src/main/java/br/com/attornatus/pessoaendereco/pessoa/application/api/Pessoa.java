3164c1feba590b7bd253c06c2183fc3997eb7018
joan <https://github.com/wakander-joan/Teste.git>
2023-01-16 22:08:56 -0300

    entidade do dominio implementada

---
+++ src/main/java/br/com/attornatus/pessoaendereco/pessoa/application/api/Pessoa.java
@@ -0,0 +1,30 @@
+package br.com.attornatus.pessoaendereco.pessoa.application.api;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Entity
+public class Pessoa {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(columnDefinition = "uuid", name = "id", updatable = false, unique = true, nullable = false)
+	private UUID idPessoa;
+	@NotBlank
+	private String nomePessoa;
+	@NotNull
+	private LocalDate dataNascimento;
+}
